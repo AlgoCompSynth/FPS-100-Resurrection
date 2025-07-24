a994db07d13e72eec6905fc90a16aa1ce1a444fb
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-07-23 20:48:25 -0700

    add an 'omega' admin user

--- PiOS-PiZero2W/2_terminal_setup.sh
+++ PiOS-PiZero2W/2_terminal_setup.sh
@@ -25,4 +25,19 @@ echo "Installing Starship"
 echo "Adding Starship prompt to bash"
 echo 'eval "$(starship init bash)"' >> $HOME/.bashrc
 
+echo "Adding 'omega' admin user"
+sudo useradd \
+  --comment "System Administrator" \
+  --create-home \
+  --groups sudo \
+  --shell /usr/bin/bash \
+  --user-group \
+  omega
+
+echo "Enter a password for the 'omega' user"
+sudo passwd omega
+
+echo "Enabling passwordless 'sudo' for 'omega'"
+echo "omega ALL=(ALL) NOPASSWD: ALL" | sudo tee --append /etc/sudoers.d/010_pi-nopasswd
+
 echo "Finished"
