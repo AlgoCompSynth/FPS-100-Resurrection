67de586541c46d85fc21419bbd4fc8fdaf3655ad
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-07-24 20:25:51 -0700

    add document download

---
+++ download_docs.sh
@@ -0,0 +1,36 @@
+#! /usr/bin/env bash
+
+set -e
+
+echo ""
+echo "* Download Docs *"
+
+mkdir --parents $HOME/Logfiles
+export LOGFILE=$HOME/Logfiles/download_docs.log
+rm --force $LOGFILE
+
+mkdir --parents $HOME/Docs
+pushd $HOME/Docs > /dev/null
+  echo "PiDP 11 Manual"
+  curl --silent --location --output \
+    https://obsolescence.dev/pidp11/PiDP-11_Manual.pdf
+  echo "pdp11/70 Processor Manual"
+  curl --silent --location --output \
+    https://bitsavers.trailing-edge.com/pdf/dec/pdp11/1170/PDP-11_70_Handbook_1977-78.pdf
+  echo "RSX-11M"
+  curl --silent --location --output \
+    http://bitsavers.informatik.uni-stuttgart.de/pdf/dec/pdp11/rsx11m_s/RSX11M_V3.1_Dec77/1A_System_Reference_Information/DEC-11-OMBGA-A_D_RSX-11M_Beginners_Guide_Jun77.pdf
+  curl --silent --location --output \
+    https://bitsavers.org/pdf/dec/pdp11/rsx11m_s/Pieper_RSX_A_Guide_For_Users_1987.pdf 
+  curl --silent --location --output \
+    https://bitsavers.org/pdf/dec/pdp11/rsx11m_s/RSX11M_V4.2_Jul85/AA-M476A-TK_EDT_Editor_Manual_198309.pdf
+  curl --silent --location --output \
+    https://bitsavers.org/pdf/dec/pdp11/rsx11m_s/RSX11M_V4.2_Jul85/AA-L763B-TC_Introduction_to_RSX-11M_198507.pdf
+  curl --silent --location --output \
+    https://bitsavers.org/pdf/dec/pdp11/rsx11m_s/RSX11M_V4.1_Apr83/4_ProgramDevelopment/AA-L676A-TC_gdPgmDevel_198111.pdf
+  echo "PDP-11 Fortran IV"
+  curl --silent --location --output \
+    https://www.dmv.net/dec/pdf/pdp11fortranivlrm.pdf
+popd > /dev/null
+
+echo "* Finished Download Docs *"
