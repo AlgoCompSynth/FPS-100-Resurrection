a9cc6bbed395cdd46054d32102520440a9e1a2d5
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2025-07-25 18:58:29 -0700

    add some logging

--- download_documents.sh
+++ download_documents.sh
@@ -6,18 +6,18 @@ echo ""
 echo "* Download Documents *"
 
 mkdir --parents $HOME/Logfiles
-export LOGFILE=$HOME/Logfiles/download_docs.log
+export LOGFILE=$HOME/Logfiles/download_documents.log
 rm --force $LOGFILE
 
 mkdir --parents $HOME/Documents
 pushd $HOME/Documents > /dev/null
-  echo "PiDP 11 Manual"
+  echo "PiDP 11 Manual" | tee --append $LOGFILE
   curl --silent --location --remote-name \
     https://obsolescence.dev/pidp11/PiDP-11_Manual.pdf
-  echo "pdp11/70 Processor Manual"
+  echo "pdp11/70 Processor Manual" | tee --append $LOGFILE
   curl --silent --location --remote-name \
     https://bitsavers.trailing-edge.com/pdf/dec/pdp11/1170/PDP-11_70_Handbook_1977-78.pdf
-  echo "RSX-11M"
+  echo "RSX-11M manuals" | tee --append $LOGFILE
   curl --silent --location --remote-name \
     http://bitsavers.informatik.uni-stuttgart.de/pdf/dec/pdp11/rsx11m_s/RSX11M_V3.1_Dec77/1A_System_Reference_Information/DEC-11-OMBGA-A_D_RSX-11M_Beginners_Guide_Jun77.pdf
   curl --silent --location --remote-name \
@@ -28,7 +28,7 @@ pushd $HOME/Documents > /dev/null
     https://bitsavers.org/pdf/dec/pdp11/rsx11m_s/RSX11M_V4.2_Jul85/AA-L763B-TC_Introduction_to_RSX-11M_198507.pdf
   curl --silent --location --remote-name \
     https://bitsavers.org/pdf/dec/pdp11/rsx11m_s/RSX11M_V4.1_Apr83/4_ProgramDevelopment/AA-L676A-TC_gdPgmDevel_198111.pdf
-  echo "PDP-11 Fortran IV"
+  echo "PDP-11 Fortran IV Language Reference Manual" | tee --append $LOGFILE
   curl --silent --location --remote-name \
     https://www.dmv.net/dec/pdf/pdp11fortranivlrm.pdf
 popd > /dev/null
