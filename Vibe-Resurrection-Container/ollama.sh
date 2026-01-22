bc259d3837192614efb7031f97b524484f21479e
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2026-01-21 19:03:44 -0800

    start server and pull models during build!

--- Vibe-Resurrection-Container/ollama.sh
+++ Vibe-Resurrection-Container/ollama.sh
@@ -12,8 +12,24 @@ pushd $HOME/.local > /dev/null
   echo "Extracting binaries to $HOME/.local"
   tar xf $OLLAMA_TARBALL
   echo "Creating 'ollama-serve' command"
-  echo 'ollama serve > $HOME/Logfiles/ollama.log 2>&1 &' > ./bin/ollama-serve
+  echo 'ollama serve > $LOGFILES/ollama.log 2>&1 &' > ./bin/ollama-serve
   chmod +x ./bin/ollama-serve
+  echo "Starting ollama server"
+  ./bin/ollama-serve
+  echo "Waiting 30 seconds for startup"
+  sleep 30
+
+  for model in \
+    glm-4.7-flash \
+    deepseek-coder-v2 \
+    qwen3-coder
+
+  do
+    echo "pulling $model"
+    /usr/bin/time ollama pull $model \
+      > $LOGFILES/$model.log 2>&1
+
+  done
   
 popd > /dev/null
 
